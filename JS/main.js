04bdc1bd499309565c600489c6057ac2486994b9
SEOB <svj7531@naver.com>
2024-11-12 18:37:33 +0900

    feat: main.js 에서 header 관련 코드 분리
    feat: main.js 에서 header 관련 코드 분리

--- JS/main.js
+++ JS/main.js
@@ -46,51 +46,10 @@ document.addEventListener('DOMContentLoaded', async function () {
 
   });
 
-  // 로그인 상태 확인
-  const loginBtn = document.getElementById('loginBtn');
-  const registerBtn = document.getElementById('registerBtn');
-  const myInfoBtn = document.getElementById('myInfoBtn');
-  const logoutBtn = document.getElementById('logoutBtn');
-  const chatbotBtn = document.getElementById('chatbotBtn');
-
   if (accessToken) {
-      myInfoBtn.style.display = 'inline';
-      logoutBtn.style.display = 'inline';
-      chatbotBtn.style.display = 'inline'
       pets = await loadRegisteredPets();
-  } else {
-      loginBtn.style.display = 'inline';
-      registerBtn.style.display = 'inline';
-  }
-
-  // 로그인 버튼 클릭 이벤트
-  loginBtn.addEventListener('click', function () {
-      window.location.href = 'user/login.html';
-  });
-
-  // 회원가입 버튼 클릭 이벤트
-  registerBtn.addEventListener('click', function () {
-      window.location.href = 'user/signup.html';
-  });
-
-  // 내 정보 버튼 클릭 이벤트
-  myInfoBtn.addEventListener('click', function () {
-      window.location.href = 'pet/petlist.html';
-  });
-
-  // 챗봇
-  chatbotBtn.addEventListener('click', function () {
-    window.location.href = './chatbot.html';
-  });
-
-
-  // 로그아웃 버튼 클릭 이벤트
-  logoutBtn.addEventListener('click', function () {
-      localStorage.removeItem('accessToken');
-      alert('로그아웃되었습니다.');
-      window.location.reload();
-  });
-
+  } 
+  
   // 반려동물 이미지 로드 함수
   async function loadRegisteredPets() {
       try {
